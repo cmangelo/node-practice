04221b005fb115cc229787149900f003291223fb
cmangelo <christianangelo112@msn.com>
2019-05-01 18:20:12 -0700

    Create handlebars templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 // Define path for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Handlebars config
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static views for directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -39,9 +42,26 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
-        helpText: 'This is the help page'
-    })
-})
+        helpText: 'This is the help page',
+        name: 'Christian'
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: 'Error',
+        error: 'Help article not found',
+        name: 'Christian'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'Error',
+        error: 'Page not found',
+        name: 'Christian'
+    });
+});
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
